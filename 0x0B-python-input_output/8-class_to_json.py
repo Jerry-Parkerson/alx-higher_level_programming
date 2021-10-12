8404c75e8c8a9693c49f1d5b6968b66073e7be71
Jerry-Parkerson <Paulomoragbon750@gmail.com>
2021-10-12 06:05:06 -0700

    python input/output

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""function that returns the dictionary description with simple data structure
+"""
+
+
+def class_to_json(obj):
+    """
+    Def to show every content of a class
+    :param obj: Instance of a Class
+    :return: Dictionary description of a class
+    """
+    return obj.__dict__
